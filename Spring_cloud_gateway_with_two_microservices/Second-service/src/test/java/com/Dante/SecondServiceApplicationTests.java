4c525de7f855194b9c1e232f0e63673777ff875a
devdattakhoche <49261633+devdattakhoche@users.noreply.github.com>
2019-12-10 21:46:17 +0530

    Spring Cloud api-=gatewway

---
+++ Spring_cloud_gateway_with_two_microservices/Second-service/src/test/java/com/Dante/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
