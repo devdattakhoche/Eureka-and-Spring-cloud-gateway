717f0c752880234551d17f213bf70124259d9ac7
devdattakhoche <49261633+devdattakhoche@users.noreply.github.com>
2019-12-10 21:44:14 +0530

    Eureka server with 1 client

---
+++ Eureka-Server-with_1_client_and_1_service/payment-service/src/main/java/com/dante/spring/eureka/provider/controller/PaymentController.java
@@ -0,0 +1,19 @@
+package com.dante.spring.eureka.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment-provider")
+public class PaymentController {
+		
+	    @GetMapping("/payNow/{price}")
+		public String payNow(@PathVariable int price){
+			return "Payment with "+price+" is successfull";
+		}
+}
+
+
+
